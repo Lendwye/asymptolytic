802596898e2cb3dd3234c633984e0f513c8b4107
Lendwye <d.gantsev358@gmail.com>
2025-03-26 23:17:56 +0300

    version 2. added better version of debug_info and very stupid method to get asympto difficulty of cycles

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 import clang.cindex
-clang.cindex.Config.set_library_path('C:')
-index = clang.cindex.Index.create()
-name = "main.cpp"
-translation_unit = index.parse(name, args=['-std=c++17'])
-for_amount = 0
-for node in translation_unit.cursor.walk_preorder():
-    try:
-        print(node.kind)
+
+asympto = []
+
+def debug_info(node, depth):
+    indent = '  ' * depth
+    print(f"{indent}{node.kind.name}: {node.spelling or node.displayname}")
+
+def walk_ast(node, depth=0, met_cycles=False):
+    debug_info(node, depth)
+    if node.kind == clang.cindex.CursorKind.FOR_STMT and met_cycles:
+        asympto.append("O(n^2)")
+        return True
+    res = False
+    for child in node.get_children():
         if node.kind == clang.cindex.CursorKind.FOR_STMT:
-            for_amount += 1
-    except:
-        pass
-print(for_amount)
+            res = res or walk_ast(child, depth + 1, True)
+        else:
+            res = res or walk_ast(child, depth + 1, met_cycles)
+    if node.kind == clang.cindex.CursorKind.FOR_STMT and not res:
+        asympto.append("O(n)")
+    return res
+
+def parse_code(filename):
+    clang.cindex.Config.set_library_path('C:')
+    index = clang.cindex.Index.create()
+    tu = index.parse(filename)
+    for_tree = dict()
+    walk_ast(tu.cursor)
+
+parse_code('main.cpp')
+print(asympto)
